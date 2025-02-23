1fe0da9bcc5d3af370a65df0bd9467d797d6ff2d
Gokcenaz Torgan <gokcetorgan@gmail.com>
2025-02-23 14:02:13 +0300

    added public classes

--- server/routes/main.js
+++ server/routes/main.js
@@ -2,8 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('',(req,res) => {
-    res.render('index');
+router.get('/about',(req,res) => {
+    res.render('about');
 });
+
+router.get('',(req,res) => {
+    const locals = 
+    {
+        title: "NodeJs Blog",
+        description: "Simple Blog created with Nodejs, Express and MongoDB."
+    }
+
+    res.render('index', { locals });
+});
+
+
 
 module.exports = router;
