02e7cf438f6d61196dfc2676c51d2f9939c7504b
Gokcenaz Torgan <gokcetorgan@gmail.com>
2025-02-26 19:38:58 +0300

    updated viewoldposts  button

--- server/routes/main.js
+++ server/routes/main.js
@@ -10,22 +10,62 @@ router.get('/about',(req,res) => {
 });
 
 router.get('', async (req,res) => {
-    const locals = 
+    
+    try {
+        const locals = 
     {
         title: "NodeJs Blog",
         description: "Simple Blog created with Nodejs, Express and MongoDB."
     
     }
 
-    try {
-        const data = await Post.find();
-        res.render('index', { locals , data});
+
+    let perPage = 10;
+    let page = req.query.page || 1;
+
+    const data = await Post.aggregate([{ $sort: { createdAt : -1}}])
+    .skip(perPage * page - perPage)
+    .limit(perPage)
+    .exec();
+        
+        
+    const count = await Post.countDocuments();
+    const nextPage = parseInt(page) +1;
+    const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+
+        res.render('index', 
+        {
+            
+            locals , 
+            data,
+            curent: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
     } catch (error) {
         console.log(error);
     }
 
     
 });
+
+// router.get('', async (req,res) => {
+//     const locals = 
+//     {
+//         title: "NodeJs Blog",
+//         description: "Simple Blog created with Nodejs, Express and MongoDB."
+    
+//     }
+
+//     try {
+//         const data = await Post.find();
+//         res.render('index', { locals , data});
+//     } catch (error) {
+//         console.log(error);
+//     }
+
+    
+// });
 
 
 // function insertPostData(){
